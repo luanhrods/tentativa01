8142d8ca994027cdac444423c68afa4ec7955b9c
Luan Chicale <84470000+luanhrods@users.noreply.github.com>
2024-11-11 10:33:19 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,24 +11,15 @@ export default defineConfig({
   image: {
     domains: ["images.unsplash.com"],
   },
-  // i18n: {
-  //   defaultLocale: "en",
-  //   locales: ["en", "fr"],
-  //   fallback: {
-  //     fr: "en",
-  //   },
-  //   routing: {
-  //     prefixDefaultLocale: false,
-  //   },
-  // },
   prefetch: true,
   integrations: [
     tailwind(),
     sitemap({
       i18n: {
-        defaultLocale: "en", // All urls that don't contain `fr` after `https://screwfast.uk/` will be treated as default locale, i.e. `en`
+        defaultLocale: "pt", // Define o português como idioma padrão
         locales: {
-          en: "en", // The `defaultLocale` value must present in `locales` keys
+          pt: "pt", // Configura o português como idioma principal
+          en: "en",
           fr: "fr",
         },
       },
@@ -36,29 +27,26 @@ export default defineConfig({
     starlight({
       title: "ScrewFast Docs",
       defaultLocale: "root",
-      // https://github.com/withastro/starlight/blob/main/packages/starlight/CHANGELOG.md
-      // If no Astro and Starlight i18n configurations are provided, the built-in default locale is used in Starlight and a matching Astro i18n configuration is generated/used.
-      // If only a Starlight i18n configuration is provided, an equivalent Astro i18n configuration is generated/used.
-      // If only an Astro i18n configuration is provided, the Starlight i18n configuration is updated to match it.
-      // If both an Astro and Starlight i18n configurations are provided, an error is thrown.
       locales: {
         root: {
           label: "Português",
-          lang: "en",
+          lang: "pt",
         },
+        en: { label: "English", lang: "en" },
+        fr: { label: "Français", lang: "fr" },
         de: { label: "Deutsch", lang: "de" },
         es: { label: "Español", lang: "es" },
         fa: { label: "Persian", lang: "fa", dir: "rtl" },
-        fr: { label: "English", lang: "fr" },
         ja: { label: "日本語", lang: "ja" },
         "zh-cn": { label: "简体中文", lang: "zh-CN" },
       },
-      // https://starlight.astro.build/guides/sidebar/
       sidebar: [
         {
           label: "Quick Start Guides",
           translations: {
+            pt: "Guias Rápidos",
             de: "Schnellstartanleitungen",
+            en: "Quick Start Guides",
             es: "Guías de Inicio Rápido",
             fa: "راهنمای شروع سریع",
             fr: "Guides de Démarrage Rapide",
