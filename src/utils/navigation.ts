da024d17e76c65cd72cef649974dbdcfbbbf9f68
Luan Chicale <84470000+luanhrods@users.noreply.github.com>
2024-11-11 09:06:52 -0500

    Update navigation.ts

--- src/utils/navigation.ts
+++ src/utils/navigation.ts
@@ -4,31 +4,35 @@ const navBarLinks = [
   { name: "Produtos", url: "/products" },
   { name: "Serviços", url: "/services" },
   { name: "Blog", url: "/blog" },
-  { name: "Fale conosco", url: "/contact" },
+  { name: "Fale Conosco", url: "/contact" },
 ];
 // An array of links for footer
 const footerLinks = [
   {
     section: "Ecossistema",
     links: [
-      { name: "Termos de Privacidade", url: "/welcome-to-docs/" },
-      { name: "Nossos produtos", url: "/products" },
-      { name: "Nossos serviços", url: "/services" },
+      { name: "Termos & Privacidade", url: "/welcome-to-docs/" },
+      { name: "Nossos Produtos", url: "/products" },
+      { name: "Nossos Serviços", url: "/services" },
     ],
   },
   {
-    section: "Sobre a Enchantè Digital",
+    section: "Enchantè Digital",
     links: [
-      { name: "Sobre Nós", url: "#" },
+      { name: "Nossa História", url: "#" },
       { name: "Blog", url: "/blog" },
       { name: "Carreiras", url: "#" },
+      { name: "Avaliações", url: "#" },
     ],
   },
 ];
 // An object of links for social icons
 const socialLinks = {
   facebook: "https://www.facebook.com/",
-  x: "https://twitter.com/enchantedigital",
+  x: "https://twitter.com/",
+  github: "https://github.com/mearashadowfax/ScrewFast",
+  google: "https://www.google.com/",
+  slack: "https://slack.com/",
 };
 
 export default {
